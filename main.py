49c795a95581892a72dbc855d32d60061743e996
rupesh9999 <rupesh.b500@gmail.com>
2025-08-30 09:45:20 +0000

    added division

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("add_integers")
 
+class MCPError(Exception):
+    def __init__(self, code: int, message: str):
+        self.code = code
+        self.message = message
+        super().__init__(f"code={code}, message={message}")
 @mcp.tool()
-def add(a: int, b: int) -> int:
+def divide(a: int, b: int) -> float:
     '''
     ADD TWO INTEGERS AND RETURN THE RESULT.
 
     Args:
-        a: First integer.
-        b: Second integer.
+        a: The numerator.
+        b: The denominator.
 
     Returns:
-        The sum of the a and b.
+        The result of the division.
     '''
-    return a + b
-
-if __name__ == "__main__":
-    mcp.run(transport='stdio')
+    if b == 0:
+        raise MCPError(code=400, message="Division by zero is not allowed.")
+    return a / b
